2242d2eb7950809e92bc1f56ae9e45dbb7186454
Julia Baptista <baptista.julia@yahoo.com.br>
2022-10-16 20:15:40 -0300

    feat: Implementacao das configuracoes do JPA e spring security.

--- src/main/java/com/trybe/accjava/desafiofinal/dronefeeder/DronefeederApplication.java
+++ src/main/java/com/trybe/accjava/desafiofinal/dronefeeder/DronefeederApplication.java
@@ -2,12 +2,14 @@ package com.trybe.accjava.desafiofinal.dronefeeder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.trybe.accjava.desafiofinal.dronefeeder.model.Drone;
 
 @SpringBootApplication
 public class DronefeederApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DronefeederApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DronefeederApplication.class, args);
+
+  }
 
 }
