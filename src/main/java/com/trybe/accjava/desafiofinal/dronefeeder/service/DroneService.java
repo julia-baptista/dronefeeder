0619529644af1d0a2ae2bcdb7c16c55f176190c3
Julia Baptista <baptista.julia@yahoo.com.br>
2022-10-23 21:21:25 -0300

    fix: Ajuste do método cadastrar no DroneService e no DroneServiceTest para utilizar o DTO.

--- src/main/java/com/trybe/accjava/desafiofinal/dronefeeder/service/DroneService.java
+++ src/main/java/com/trybe/accjava/desafiofinal/dronefeeder/service/DroneService.java
@@ -1,6 +1,8 @@
 package com.trybe.accjava.desafiofinal.dronefeeder.service;
 
 import org.springframework.stereotype.Service;
+import com.trybe.accjava.desafiofinal.dronefeeder.dtos.DroneDTO;
+import com.trybe.accjava.desafiofinal.dronefeeder.enums.StatusDroneEnum;
 import com.trybe.accjava.desafiofinal.dronefeeder.exception.DroneExistenteException;
 import com.trybe.accjava.desafiofinal.dronefeeder.exception.ErroInesperadoException;
 import com.trybe.accjava.desafiofinal.dronefeeder.model.Drone;
@@ -19,13 +21,14 @@ public class DroneService {
   /**
    * Cadastrar.
    */
-  public Drone cadastrar(Drone drone) {
+  public DroneDTO cadastrar(DroneDTO dto) {
 
     try {
-      if (this.droneRepository.existsByNome(drone.getNome())) {
+      if (this.droneRepository.existsByNome(dto.getNome())) {
         throw new DroneExistenteException();
       }
-      return this.droneRepository.save(drone);
+      Drone drone = this.droneRepository.save(convertDroneDtoToDrone(dto));
+      return convertDroneToDroneDTO(drone);
     } catch (Exception e) {
       if (e instanceof DroneExistenteException) {
         throw (DroneExistenteException) e;
@@ -33,4 +36,26 @@ public class DroneService {
       throw new ErroInesperadoException();
     }
   }
+
+  private Drone convertDroneDtoToDrone(DroneDTO dto) {
+    Drone drone = new Drone(dto.getNome(), dto.getMarca(), dto.getFabricante(),
+        dto.getAltitudeMax(), dto.getDuracaoBateria(), dto.getCapacidadeKg(), dto.getCapacidadeM3(),
+        StatusDroneEnum.ATIVO);
+    return drone;
+  }
+
+  private DroneDTO convertDroneToDroneDTO(Drone drone) {
+    return DroneDTO
+      .builder()
+      .id(drone.getId())
+      .nome(drone.getNome())
+      .marca(drone.getMarca())
+      .fabricante(drone.getFabricante())
+      .altitudeMax(drone.getAltitudeMax())
+      .duracaoBateria(drone.getDuracaoBateria())
+      .capacidadeKg(drone.getCapacidadeKg())
+      .capacidadeM3(drone.getCapacidadeM3())
+      .status(drone.getStatus().getStatus())
+      .build();
+    }
 }
--- src/test/java/com/trybe/accjava/desafiofinal/dronefeeder/service/DroneServiceTest.java
+++ src/test/java/com/trybe/accjava/desafiofinal/dronefeeder/service/DroneServiceTest.java
@@ -1,5 +1,6 @@
 package com.trybe.accjava.desafiofinal.dronefeeder.service;
 
+import static org.mockito.Mockito.times;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,7 +8,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.trybe.accjava.desafiofinal.dronefeeder.dtos.DroneDTO;
+import com.trybe.accjava.desafiofinal.dronefeeder.enums.StatusDroneEnum;
 import com.trybe.accjava.desafiofinal.dronefeeder.exception.DroneExistenteException;
+import com.trybe.accjava.desafiofinal.dronefeeder.exception.ErroInesperadoException;
 import com.trybe.accjava.desafiofinal.dronefeeder.model.Drone;
 import com.trybe.accjava.desafiofinal.dronefeeder.repository.DroneRepository;
 
@@ -20,22 +24,28 @@ public class DroneServiceTest {
   @InjectMocks
   private DroneService service;
 
+  DroneDTO dto = new DroneDTO();
+
   Drone drone = new Drone();
 
   @BeforeEach
   public void setup() {
-    drone = new Drone();
+    dto = DroneDTO.builder().id(1L).nome("Drone A").fabricante("Drone Technology").build();
+
     drone.setId(1L);
     drone.setNome("Drone A");
-    drone.setFabricante("Natives drone");
+    drone.setFabricante("Drone Technology");
+    drone.setStatus(StatusDroneEnum.ATIVO);
   }
 
   @Test()
   public void shouldCadastrarNovoDrone() {
+
+
     Mockito.when(repository.existsByNome(Mockito.anyString())).thenReturn(false);
     Mockito.when(repository.save(Mockito.any())).thenReturn(drone);
 
-    Drone result = service.cadastrar(drone);
+    DroneDTO result = service.cadastrar(dto);
 
     Assertions.assertNotNull(result);
     Assertions.assertEquals("Drone A", result.getNome());
@@ -47,10 +57,25 @@ public class DroneServiceTest {
 
     // https://stackoverflow.com/questions/40268446/junit-5-how-to-assert-an-exception-is-thrown
     DroneExistenteException exception =
-        Assertions.assertThrows(DroneExistenteException.class, () -> service.cadastrar(drone));
+        Assertions.assertThrows(DroneExistenteException.class, () -> service.cadastrar(dto));
 
+    Mockito.verify(repository, times(0)).save(Mockito.any());
     Assertions.assertNotNull(exception);
     Assertions.assertEquals("Drone Existente", exception.getMessage());
 
+  }
+
+  @Test
+  public void shouldFailCadastrarNovoDroneWhenSomeUnexpectedErrorOccurs() {
+    Mockito.when(repository.existsByNome(Mockito.anyString())).thenReturn(false);
+    Mockito.when(repository.save(Mockito.any())).thenThrow(new IllegalArgumentException());
+
+    ErroInesperadoException exception =
+        Assertions.assertThrows(ErroInesperadoException.class, () -> service.cadastrar(dto));
+
+    Mockito.verify(repository, times(1)).existsByNome(Mockito.anyString());
+    Mockito.verify(repository, times(1)).save(Mockito.any());
+    Assertions.assertNotNull(exception);
+    Assertions.assertEquals("Erro inesperado", exception.getMessage());
   }
 }
